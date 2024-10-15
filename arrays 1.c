74fc0829d0d9f814666f53222c75aaefcfbd9458
keshiindungi <joan03267@gmail.com>
2024-10-15 14:42:29 +0300

    1D array

---
+++ arrays 1.c
@@ -0,0 +1,17 @@
+//c arrays-1D arrays
+/*
+c arrays-1D
+Name:Joan Wanjiru
+Reg:CT101/G/21648/24
+Date:15/10/24
+*/
+#include<stdio.h>
+int main (){
+	int i ;
+	int marks[5] = {50,60,70,80,2};
+	for(i=0;i<5;i++){
+		printf("marks[%d]%d\n",i,marks[i]);
+	}
+	
+	return 0;
+}
